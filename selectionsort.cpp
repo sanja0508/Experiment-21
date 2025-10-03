aa904b68f9c0790a000e14110b6400048659f101
Sanjana Anuj Pawar <pawarsanjana2006@gmail.com>
2025-10-03 00:06:12 -0400

    Create selectionsort.cpp

---
+++ selectionsort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for(int j = i+1; j < n; j++)
+            if(arr[j] < arr[min_idx])
+                min_idx = j;
+        swap(arr[i], arr[min_idx]);
+    }
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
