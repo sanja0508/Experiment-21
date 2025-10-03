34360a35575f5bc5ccafeaf07798a3694694aaee
Sanjana Anuj Pawar <pawarsanjana2006@gmail.com>
2025-10-03 00:07:15 -0400

    Create bubblesort.cpp

---
+++ bubblesort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
+        }
+    }
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
